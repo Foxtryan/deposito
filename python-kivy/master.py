edb7d9b213a7b50fea15526546ca17471aded5ad
Rafael <rafamf2.2@gmail.com>
2017-02-02 17:56:12 -0200

    Add files via upload

---
+++ python-kivy/master.py
@@ -0,0 +1,47 @@
+#coding: utf-8
+#author: Rafael "Foxtryan" Franco
+#Complemento: Tabela
+#Last Update: 26/01/2017
+
+#Empresa: Master Temper
+
+
+# ------------------ Tabela ------------------# 
+''' Vidros '''
+# Box
+Box_Inc       = 67.00
+Box_Fume      = 79.00
+# Engenharia
+Eng8_Inc      = 82.00
+Eng8_Fume     = 110.00
+Eng8_Bronze   = 150.00
+Eng10_Inc     = 110.00
+Eng10_Fume    = 142.00
+Eng10_Bronze  = 182.00
+''' Kit Box '''
+F1_120        = 95.00
+F1_133        = 105.00
+F1_150        = 112.00
+F1_180        = 121.00
+'''Aluminio'''
+akg           = 22        # Valor do alumínio
+ajl           = 1.6       # Aluminio janela largura (Kg/m)
+ajh           = 0.965     # Alumínio janela Altura (Kg/m)
+apl           = 1.7       # Porta de correr largura (Kg/m)
+aph           = 0.985     # Porta de correr altura (Kg/m)
+apal          = 4.5       # OBS Portas de Correr - Cálculo de Alumínio = perímetro * 3
+#apah         = 
+'''Acessórios'''
+ajanela4f      = 45
+ajanela2f      = 40
+aportacorrer4f = 110
+aportacorrer2f = 75
+aportaabrirg   = 205
+aportaabrirp   = 160
+#Separar cálculos de maxin-ar e basculante.
+amaxg          = 75       # Basc / maxinar 
+amaxp          = 55       # Basc / maxinar
+abasc          = 6        # Basc / maxinar
+''' Ganho '''
+Box_Ganho     = 1.70 # 70%
+Eng_Ganho     = 1.45 # 45%
