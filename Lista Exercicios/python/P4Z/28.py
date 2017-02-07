e097d7d4289645af8578fde9514e984d0a3c750a
Rafael <rafamf2.2@gmail.com>
2017-02-07 11:55:42 -0200

    Delete 28.py

--- Lista Exercicios/python/P4Z/28.py
+++
@@ -1,6 +0,0 @@
-'''
-Conversor de decimal para romano. Você deverá programar um algoritmo em Pthon que
-traduza um número lido no sistema arábico para romano. Evite fazer muitos "ifs". 
-A ideia é usar um comando while para analisar cada casa decimal e gerar os caracteres 
-romanos diferentemente para caad iteração. Exemplo 2011 = MMXI em romano. Não precisa testar até o infinito, basta de 1 até 2020.
-'''
