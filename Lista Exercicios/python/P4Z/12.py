80348ba9693dd0fd05f57cf15f38a9100ad59c5f
Rafael <rafamf2.2@gmail.com>
2017-02-07 11:50:50 -0200

    Delete 12.py

--- Lista Exercicios/python/P4Z/12.py
+++
@@ -1,5 +0,0 @@
-'''
-Faça um programa que peça os três lados de um triãngulo. O programa deverá informar
-se os valors podem ser um triãngulo. Indique, caso os lados formem um triângulo, se o 
-mesmo é: equilátero, isósceles ou escaleno.
-'''
