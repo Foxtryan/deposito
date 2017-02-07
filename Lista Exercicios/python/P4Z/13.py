4b6aa602c1c6d2127c3b8137862cba9d7bad6644
Rafael <rafamf2.2@gmail.com>
2017-02-07 11:52:28 -0200

    Delete 13.py

--- Lista Exercicios/python/P4Z/13.py
+++
@@ -1,6 +0,0 @@
-'''
-Determine se um ano é bissexto.
-Lembre-se que para um ano ser bissexto ele deve:
-- Ser dividível por 4. Sendo assim, a divisão exata com o resto igual a zero;
-- Não pode ser divisível por 100. Ou seja, resto diferente de zero;
-'''
