a2c60db79efe8c5129d85b274108dda6b6a44ee3
Rafael <rafamf2.2@gmail.com>
2017-02-07 11:53:40 -0200

    Delete 17.py

--- Lista Exercicios/python/P4Z/17.py
+++
@@ -1,7 +0,0 @@
-'''
-Faça um progrmaa para uma loja de tintas. O programa deverá pedir o tamanho em metros 
-quadrados da área a ser pintada. considere que a cobertura da tinta é de 1 litro para 
-cada 3 metros quadrados e que a tinta é vendida em latas de 18 listros, que custam 
-R$: 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o 
-preço total. Obs.: somente são vendidos um número inteiro de latas. 
-'''
