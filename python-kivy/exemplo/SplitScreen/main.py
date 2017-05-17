9a8384edd7741fdc586a566346d8cd597a444ede
Rafael <rafamf2.2@gmail.com>
2017-05-17 13:52:08 -0300

    Add files via upload

---
+++ python-kivy/exemplo/SplitScreen/main.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+
+class Principal(BoxLayout):
+    pass
+class Primeira(Screen):
+    pass
+class Segunda(Screen):
+	pass
+class Comparativo(Screen):
+	pass
+
+class TesteApp(App):
+    def build(self):
+        return Principal()
+
+if __name__ == "__main__":
+    TesteApp().run()
