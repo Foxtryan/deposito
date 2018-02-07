b5ed7478ad50bef911e57e22301028ef8303609a
Rafael <rafamf2.2@gmail.com>
2018-02-07 11:57:50 -0200

    Update ex.py

--- ex.py
+++ ex.py
@@ -46,13 +46,13 @@ def Dentro():
 def Fora():
 	App.get_running_app().texto = "Fora do Estado"
 
-menu_tipoVenda = [
-	{'viewclass': 'MDMenuItem', 'text': 'Dentro do Estado', 'on_press':Dentro},
-	{'viewclass': 'MDMenuItem', 'text': 'Fora do Estado', 'on_press':Fora},
-]
+
 	
 class TelaEmissor(Screen):
-
+	menu_tipoVenda = [
+		{'viewclass': 'MDMenuItem', 'text': 'Dentro do Estado', 'on_press':Dentro},
+		{'viewclass': 'MDMenuItem', 'text': 'Fora do Estado', 'on_press':Fora},
+	]
 	class btnTipoVenda(MDFlatButton):
 
 		def __init__(self, **kwargs):
